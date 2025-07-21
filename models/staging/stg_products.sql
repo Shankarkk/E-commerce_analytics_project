c06405ed046cb654bfc648bc7b577ffa660ffaf9
Shankarkk <mailbox869115@gmail.com>
2025-07-21 20:02:08 +0530

    Create stg_products.sql
    Added stg_products

---
+++ models/staging/stg_products.sql
@@ -0,0 +1,12 @@
+with source as (
+    select * from {{ source('datafeed', 'raw_products') }}
+)
+
+select
+    product_id::varchar as product_id,
+    product_name::varchar as product_name,
+    category::varchar as category,
+    brand::varchar as brand,
+    cast(unit_price as float) as unit_price,
+    availability::varchar as availability
+from source
