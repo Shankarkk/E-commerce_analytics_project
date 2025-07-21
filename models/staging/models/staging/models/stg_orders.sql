e4e713117a8e2dcf19a891e0fda4fcccacb06e67
Shankarkk <mailbox869115@gmail.com>
2025-07-21 07:06:01 +0530

    Create stg_orders.sql
    Added staging models (structure: models/staging/): stg_customers, stg_orders, stg_payments

---
+++ models/staging/models/staging/models/stg_orders.sql
@@ -0,0 +1,15 @@
+-- Path: models/staging/stg_order.sql
+{{ config(materialized='view', tags=['staging']) }}
+
+with stg_orders as (
+    select 
+        order_id,
+        customer_id,
+        order_date::date as order_date,
+        order_amount,
+        status,
+        created_at::timestamp as order_created_at,
+        updated_at::timestamp as order_updated_at
+    from {{ source('datafeed', 'raw_orders') }}
+)
+select * from stg_orders;
