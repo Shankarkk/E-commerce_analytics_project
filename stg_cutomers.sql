b5dde4476a2fe29b93370a7137f91c0e7954ca10
Shankarkk <mailbox869115@gmail.com>
2025-07-21 12:14:39 +0530

    Update stg_cutomers.sql
    Update stg_customers path

--- stg_cutomers.sql
+++ stg_cutomers.sql
@@ -1,1 +1,16 @@
-path:models/staging/stg.customers
+--path:models/staging/stg.customers
+{{ config(materialized='view', tags=['staging']) }}
+
+with stg_customers as (
+    select 
+        customer_id,
+        first_name,
+        last_name,
+        email,
+        gender,
+        created_at::timestamp as customer_created_at,
+        updated_at::timestamp as customer_updated_at
+    from {{ source('datafeed', 'raw_customer') }}
+)
+select * from stg_customers;
+
