0fa98e33625fcc9c75d9a25cd4ff4b63d9b24cea
Shankarkk <mailbox869115@gmail.com>
2025-07-21 14:43:30 +0530

    Create int_rfm.sql
    Added int_rfm.sql

---
+++ models/intermediate/int_rfm.sql
@@ -0,0 +1,17 @@
+-- models/intermediate/int_rfm.sql
+
+with customer_orders as (
+    select
+        sc.customer_id,
+        max(so.order_date) as most_recent_order,
+        datediff('day', max(so.order_date), current_date) as recency,
+        count(so.order_id) as frequency,
+        sum(so.order_amount) as contribution
+    from {{ ref('stg_customers') }} as sc
+    join {{ ref('stg_orders') }} as so
+        on sc.customer_id = so.customer_id
+    group by sc.customer_id
+)
+
+select *
+from customer_orders
