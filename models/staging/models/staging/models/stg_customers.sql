e9c02521d471a87753e6afc06948c05d53b0b49e
Shankarkk <mailbox869115@gmail.com>
2025-07-21 07:01:25 +0530

    Create stg_customers.sql
    Added staging models (structure: models/staging/): stg_customers, stg_orders, stg_payments

---
+++ models/staging/models/staging/models/stg_customers.sql
@@ -0,0 +1,15 @@
+-- Path: models/staging/stg_customers.sql
+{{ config(materialized='view', tags=['staging']) }}
+
+with stg_customers as (
+    select 
+        customer_id,
+        first_name,
+        last_name,
+        email,
+        gender,
+        created_at::timestamp as customer_created_at,
+        updated_at::timestamp as customer_updated_at
+    from {{ source('datafeed', 'raw_customer') }}
+)
+select * from stg_customers;
