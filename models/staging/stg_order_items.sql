02aa69d73dbe7849aa9314612ccbf2c8c3b71af9
Shankarkk <mailbox869115@gmail.com>
2025-07-21 20:03:31 +0530

    Create stg_order_items.sql
    Added stg_order_items.sql

---
+++ models/staging/stg_order_items.sql
@@ -0,0 +1,11 @@
+with source as (
+    select * from {{ source('datafeed', 'raw_order_items') }}
+)
+
+select
+    order_item_id::varchar as order_item_id,
+    order_id::varchar as order_id,
+    product_id::varchar as product_id,
+    cast(quantity as integer) as quantity,
+    cast(unit_price as float) as unit_price
+from source
