6852ff7cd4cd2113adbb1ca84ab921c9a507c2ff
Shankarkk <mailbox869115@gmail.com>
2025-07-21 16:35:10 +0530

    Create top_customers.sql
    Added mart top_customers.sql

---
+++ models/marts/top_customers.sql
@@ -0,0 +1,23 @@
+with ranked as (
+    select * 
+    from {{ ref('int_rfm_ranked') }}
+    where rno <= 20
+)
+
+select
+    customer_id,
+    contribution,
+    recency,
+    frequency,
+    monetary,
+    rno,
+    case
+        when recency <= 7 then 'Highly Active'
+        when recency between 8 and 30 then 'Active'
+        else 'Inactive'
+    end as customer_activity_status,
+    case
+        when contribution >= 1000 then 'High Value'
+        else 'Low Value'
+    end as customer_value_segment
+from ranked
