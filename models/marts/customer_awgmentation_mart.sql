2b9a8f2623c70060c2afafdfac832aa2da9d3c6b
Shankarkk <mailbox869115@gmail.com>
2025-07-22 12:56:38 +0530

    Create customer_awgmentation_mart.sql
    Added customer segmentation mart with churn and behavioral logic

---
+++ models/marts/customer_awgmentation_mart.sql
@@ -0,0 +1,44 @@
+-- models/marts/customer_segmentation_mart.sql
+
+{{ config(
+    materialized='table',
+    tags=['mart', 'segmentation']
+) }}
+
+with rfm_data as (
+    select * from {{ ref('int_rfm_ranked_customers') }}
+),
+
+churn_logic as (
+    select
+        *,
+        case 
+            when recency <= 30 then 'Active'
+            when recency > 30 and recency <= 90 then 'At Risk'
+            else 'Churned'
+        end as churn_status
+    from rfm_data
+),
+
+behavior_logic as (
+    select
+        *,
+        case 
+            when frequency >= 10 and monetary >= 1000 then 'Loyal High Spender'
+            when frequency >= 10 then 'Loyal Customer'
+            when monetary >= 1000 then 'High Spender'
+            else 'Occasional'
+        end as behavioral_segment
+    from churn_logic
+)
+
+select
+    customer_id,
+    recency,
+    frequency,
+    monetary,
+    rfm_score,
+    churn_status,
+    behavioral_segment,
+    churn_status || ' - ' || behavioral_segment as combined_segment
+from behavior_logic
