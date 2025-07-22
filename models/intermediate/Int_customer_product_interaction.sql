dddb2a5257ed0c178f6f412aff6d28270fb21406
Shankarkk <mailbox869115@gmail.com>
2025-07-22 07:05:24 +0530

    Create Int_customer_product_interaction.sql
    Added int_customer_production_interaction.sql

---
+++ models/intermediate/Int_customer_product_interaction.sql
@@ -0,0 +1,30 @@
+with order_items as (
+
+    select 
+        o.order_id,
+        o.customer_id,
+        i.product_id,
+        i.quantity,
+        i.amount
+    from {{ ref('stg_orders') }} o
+    join {{ ref('stg_order_items') }} i
+      on o.order_id = i.order_id
+    where o.status = 'delivered'
+
+),
+
+customer_product_metrics as (
+
+    select 
+        customer_id,
+        product_id,
+        count(distinct order_id) as total_orders,
+        sum(quantity) as total_quantity,
+        sum(amount) as total_spent_on_product
+    from order_items
+    group by customer_id, product_id
+
+)
+
+select * 
+from customer_product_metrics
